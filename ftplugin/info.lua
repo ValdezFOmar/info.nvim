5682d8c194c2b5ef192516105483b3adc2776b9e
Omar Valdez <omarantoniovaldezf2@gmail.com>
2025-05-02 23:30:41 -0700

    feat(ftplugin): add `q` mapping to close info window

--- ftplugin/info.lua
+++ ftplugin/info.lua
@@ -30,6 +30,7 @@ command(0, 'InfoFollow', function(p)
     info.follow(p.smods)
 end, {})
 
+map('n', 'q', '<C-w>c', { desc = 'Close info window', buffer = true })
 map('n', 'K', '<cmd>InfoFollow<CR>', { desc = 'Follow node reference under cursor', buffer = true })
 map('n', 'gn', '<cmd>InfoNext<CR>', { desc = 'Go to the next node', buffer = true })
 map('n', 'gp', '<cmd>InfoPrev<CR>', { desc = 'Go to the previous node', buffer = true })
