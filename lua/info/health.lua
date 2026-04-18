ce805c677ed8806712f70535614a5a6fddec855b
Omar Valdez <omarantoniovaldezf2@gmail.com>
2026-04-18 03:14:03 -0700

    refactor(health): simplify version check and formatting

--- lua/info/health.lua
+++ lua/info/health.lua
@@ -1,29 +1,32 @@
 local M = {}
-local MIN_INFO_VERSION = { 7, 0 }
 
 function M.check()
-    vim.health.start 'info.nvim'
+    vim.health.start 'System'
 
     local info_path = vim.fn.exepath 'info'
     if info_path == '' then
-        vim.health.error '`info` executable not found'
+        vim.health.error(
+            '`info` executable not found',
+            'Check that `info` is installed and available in path'
+        )
     else
-        vim.health.ok('`info` executable found (' .. info_path .. ')')
+        vim.health.ok(('Executable found: `%s`'):format(info_path))
     end
 
     local cmd = vim.system({ info_path, '--version' }, { text = true }):wait(10000)
     if cmd.code ~= 0 then
         vim.health.error('`info --version` exit with code: ' .. cmd.code)
-        return
     end
 
-    local version = vim.split(assert(cmd.stdout), '\n')[1]:match '(%d+%.%d+)'
-    if vim.version.ge(version, MIN_INFO_VERSION) then
-        vim.health.ok('supported version ' .. version)
+    local min_version = '7.0'
+    local version = assert(vim.version.parse(cmd.stdout))
+
+    if vim.version.ge(version, min_version) then
+        vim.health.ok(('Supported version: %s'):format(version))
     else
-        vim.health.warn(
-            'unsupported version ' .. version,
-            'upgrade to ' .. table.concat(MIN_INFO_VERSION, '.') .. ' or later'
+        vim.health.error(
+            ('Unsupported `info` version: %s'):format(version),
+            ('Upgrade to version %s or later'):format(min_version)
         )
     end
 end
